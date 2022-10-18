6766f11bdb288fa865dfe997bf77afd1e04b278a
Calandra Lark <anthony.abbott@calandralark.com>
2022-10-18 19:28:31 +0100

    Initial Commit

---
+++ UKContracts/UKContractsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UKContractsApp.swift
+//  UKContracts
+//
+//  Created by Anthony Abbott on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct UKContractsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
