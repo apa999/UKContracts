5c3b939aa925723b5d0621def180a5023224fc71
Calandra Lark <anthony.abbott@calandralark.com>
2022-10-21 19:09:58 +0100

    Next and Prev

--- UKContracts/AppSources/Model/Contracts/CFModel.swift
+++ UKContracts/AppSources/Model/Contracts/CFModel.swift
@@ -9,20 +9,27 @@ import Foundation
 
 struct CFModel {
   
-
-  var cfSearch = CFSearch()
-  {
-    didSet {
-      if let releases = cfSearch.releases {
-        for release in releases {
-          if let tender = release.tender {
-            if let communication = tender.communication {
-//              print(communication)
-            }
-          }
-        }
+  /// Main search objects, contauining the releases []
+  var cfSearch = CFSearch() {
+    didSet{
+      if let firstRelease = cfSearch.releases?.first {
+        self.currentRelease = firstRelease
       }
-    } // didSet
+    }
+  }
+ 
+  /// The current selected release - can be optional when user has selected a release-1
+  var currentRelease: Release? {
+    didSet {
+      print("currentRelease: \(currentRelease?.id ?? "Current release has no id")")
+    }
+  }
+  
+  //MARK: - init
+  init() {
+    if let firstRelease = cfSearch.releases?.first {
+      self.currentRelease = firstRelease
+    }
   }
   
   /// Model status
@@ -33,5 +40,31 @@ struct CFModel {
     case loadingError
   }
   
+  /// Lets the ViewModel know what state the engine is in - to control the ProgressView()
   var modelStatus = ModelStatus.unloaded
+  
+  
+  //MARK: - model updates
+  
+  /// Sets the current release to the next
+  mutating func setNextRelease() {
+    if let currentRelease = currentRelease, let releases = cfSearch.releases {
+      if let firstIndex = releases.firstIndex(where: {$0.id == currentRelease.id }) {
+        if firstIndex < releases.count - 1 {
+          self.currentRelease = releases[firstIndex + 1]
+        }
+      }
+    }
+  } // mutating func setNextRelease
+  
+  /// Sets the current release to the previous
+  mutating func setPrevRelease() {
+    if let currentRelease = currentRelease, let releases = cfSearch.releases {
+      if let firstIndex = releases.firstIndex(where: {$0.id == currentRelease.id }) {
+        if firstIndex > 0 {
+          self.currentRelease = releases[firstIndex - 1]
+        }
+      }
+    }
+  } // mutating func setPrevRelease()
 }
