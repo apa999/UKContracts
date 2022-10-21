7f5b3953bf6333fed43d41ffad903610f34296bb
Calandra Lark <anthony.abbott@calandralark.com>
2022-10-21 09:23:11 +0100

    Testing

--- UKContractsTests/CFSearch_ComponentsTests/Period_Tests.swift
+++ UKContractsTests/CFSearch_ComponentsTests/Period_Tests.swift
@@ -5,26 +5,47 @@
 //  Created by Anthony Abbott on 18/10/2022.
 //
 
+// MARK: - Period
+/*
+ struct Period : Codable {
+   let startDate : Date?
+   let endDate   : Date?
+ */
+
 import XCTest
 @testable import UKContracts
 
 final class Period_Tests: XCTestCase {
   
+  let startDate = TestHelpers.getDateFor("2022-05-19T06:00:00+0000")
+  let endDate   = TestHelpers.getDateFor("2022-10-28T18:00:00+0000")
+  let dataStr = """
+{
+"startDate": "2022-11-25T00:00:00Z",
+"endDate"  : "2025-11-24T23:59:59Z"
+}
+"""
   func test_Init() throws {
+    let sut = Period(startDate: startDate, endDate: endDate)
+    
+    XCTAssertEqual(sut.formattedStartDate, "Thu 19 May 2022, 07:00")
+    XCTAssertEqual(sut.formattedEndDate, "Fri 28 Oct 2022, 19:00")
   } 
 
+  func test_Decode() throws {
+    
+    let data = Data(dataStr.utf8)
+    
+    let sut = try TestHelpers.decode(Period.self,from: data)
+    
+    XCTAssertEqual(sut.formattedStartDate, "Fri 25 Nov 2022, 00:00")
+    XCTAssertEqual(sut.formattedEndDate, "Mon 24 Nov 2025, 23:59")
+  }
+  
   override func setUpWithError() throws {
   }
   
   override func tearDownWithError() throws {
-  }
-  
-  func testExample() throws {
-  }
-  
-  func testPerformanceExample() throws {
-    self.measure {
-    }
   }
 }
 
