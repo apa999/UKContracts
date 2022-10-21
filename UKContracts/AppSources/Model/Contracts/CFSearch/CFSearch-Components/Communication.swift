a5b0125d846cb55b8b7a0930da22fcbfeb924552
Calandra Lark <anthony.abbott@calandralark.com>
2022-10-21 10:48:58 +0100

    Added formatted data

--- UKContracts/AppSources/Model/Contracts/CFSearch/CFSearch-Components/Communication.swift
+++ UKContracts/AppSources/Model/Contracts/CFSearch/CFSearch-Components/Communication.swift
@@ -10,4 +10,16 @@ import Foundation
 // MARK: - Communication
 struct Communication : Codable {
   let futureNoticeDate : Date?
+  
+  /// Format the date
+  var formattedFutureNoticeDate: String {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "E d MMM y, HH:mm"
+    
+    if let date = futureNoticeDate {
+      return formatter.string(from: date)
+    } else {
+      return ""
+    }
+  }
 }
