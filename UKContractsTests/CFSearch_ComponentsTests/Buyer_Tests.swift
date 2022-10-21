91a9e1e13b03b21cb320c184a4df7b3f63c34ffa
Calandra Lark <anthony.abbott@calandralark.com>
2022-10-21 10:39:59 +0100

    Testing

--- UKContractsTests/CFSearch_ComponentsTests/Buyer_Tests.swift
+++ UKContractsTests/CFSearch_ComponentsTests/Buyer_Tests.swift
@@ -5,26 +5,52 @@
 //  Created by Anthony Abbott on 18/10/2022.
 //
 
+// MARK: - Buyer
+/*
+ struct Buyer: Codable, Hashable {
+   let id   : String?
+   let name : String?
+ }
+ */
+
+
 import XCTest
 @testable import UKContracts
 
 final class Buyer_Tests: XCTestCase {
   
   func test_Init() throws {
+    let sut = Buyer(id: "id", name: "name")
+    XCTAssertEqual(sut.id, "id")
+    XCTAssertEqual(sut.name, "name")
   } 
 
+  func test_Decode() throws {
+    
+    let data = Data(dataStr.utf8)
+    
+    do {
+      let sut = try TestHelpers.decode(Buyer.self,from: data)
+      
+      XCTAssertNotNil(sut)
+      XCTAssertEqual(sut.id, "GB-SRS-sid4gov.cabinetoffice.gov.uk/hWvd872d")
+      XCTAssertEqual(sut.name, "Natural England")
+    } catch {
+      XCTFail("Failed to decode: \(error)")
+    }
+  }
+  
   override func setUpWithError() throws {
   }
   
   override func tearDownWithError() throws {
   }
   
-  func testExample() throws {
-  }
-  
-  func testPerformanceExample() throws {
-    self.measure {
-    }
-  }
+  let dataStr = """
+{
+"id": "GB-SRS-sid4gov.cabinetoffice.gov.uk/hWvd872d",
+"name": "Natural England"
+}
+"""
 }
 
